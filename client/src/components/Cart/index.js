807799d3b48684cc28c6728ddd8334184f6d9668
stevore <skamau22@wustl.edu>
2023-07-06 16:16:29 -0700

    initial commit

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import './style.css';
+
