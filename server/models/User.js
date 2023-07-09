70d637cde64ca733e0bb19198bb37ac14474c0d1
Khuzema <maslam002@citymail.cuny.edu>
2023-07-08 20:42:41 -0400

    Added seed data and runs on graphql

--- server/models/User.js
+++ server/models/User.js
@@ -6,23 +6,34 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   firstName: {
     type: String,
-   // required: true,
+    required: true,
   //  trim: true,
   },
   lastName: {
     type: String,
-  //  required: true,
+    required: true,
  //   trim: true,
   },
+  userName: {
+    type: String,
+    required: true,
+ //   trim: true,
+  },
+  github: {
+    type: String,
+    required: true,
+ //   trim: true,
+  },
+
   email: {
     type: String,
- //   required: true,
-//    unique: true,
+    required: true,  
+    unique: true,
   },
   password: {
     type: String,
- //   required: true,
- //   minlength: 5,
+    required: true,
+    minlength: 5,
   },
 
 });
