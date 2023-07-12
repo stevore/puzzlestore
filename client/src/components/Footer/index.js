0d10ea6576fa95c9ec538e22c9e75606feb744b4
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-07-11 23:37:41 -0400

    updated header and footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import logoFacebook from './../../assets/images/facebook.png';
+import logoPinterest from './../../assets/images/pinterest.png';
+import logoInstagram from './../../assets/images/instagram.png';
+import logoThreads from './../../assets/images/threads.png';
 
 const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container text-center mb-5">
+    <footer className="container-fluid w-100 p-2">
+      <div className='row'> 
+      <div className="col-sm-4 col-md-4 col-lg-4">
+        About Us / Contact Us / Shipping Info
+      </div>
+           <div className=" text-center  col-sm-4 col-md-4 col-lg-4">
         {location.pathname !== '/' && (
           <button
             className="btn btn-dark mb-3"
@@ -16,6 +24,14 @@ const Footer = () => {
           </button>
         )}
       </div>
+      <div id="social" className="col-sm-4 col-md-4 col-lg-4">
+            <img src={logoFacebook} alt="Facebook" />
+            <img src={logoInstagram} alt="Instagram" />
+            <img src={logoPinterest} alt="Pinterest" />
+            <img src={logoThreads} alt="Threads" />
+      </div>
+      </div>
+
     </footer>
   );
 };
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logoCart from './../../assets/images/shoppingcart.png';
+import hamburger from './../../assets/images/hamburger.png';
 
 import Auth from '../../utils/auth';
 
@@ -9,9 +11,14 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
+    <header className="container-fluid w-100 bg-primary text-light">
+      <div className="flex-row justify-space-between-lg justify-center align-center">
         <div>
+          <Link className="" to={`/home`}>
+            <img src={hamburger} alt="Menu" />
+          </Link>
+          </div>
+          <div>
           <Link className="text-light" to="/">
             <h1 className="m-0">The Puzzle Store</h1>
           </Link>
@@ -23,20 +30,25 @@ const Header = () => {
               <Link className="btn btn-lg btn-info m-2" to="/me">
                 {Auth.getProfile().data.firstName}'s profile
               </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              <button className="btn btn-md btn-light m-2" onClick={logout}>
                 Logout
               </button>
             </>
           ) : (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
+              <Link className="btn btn-md btn-info m-2" to="/login">
                 Login
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
+              <Link className="btn btn-md btn-light m-2" to="/signup">
                 Signup
               </Link>
             </>
           )}
+        </div>
+        <div>
+        <Link className="" to={`/cart`}>
+                      <img src={logoCart} alt="Cart" /> 
+        </Link>
         </div>
       </div>
     </header>
